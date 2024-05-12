a77fe7b5e17e3268923b30b32f1940919f5a7228
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-05-12 14:59:09 +0300

    fix in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
-use App\Models\Order;
+// use App\Models\Order;
 use App\Models\Order_item;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
